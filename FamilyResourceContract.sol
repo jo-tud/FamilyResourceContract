0b664c416fe3b2b59cbf7733250c119784372300
Johannes <jo-tud@users.noreply.github.com>
2016-05-20 15:26:08 +0200

    Some improvements, use modifiers

--- FamilyResourceContract.sol
+++ FamilyResourceContract.sol
@@ -3,16 +3,13 @@ contract FamilyResourceControl{
     uint public contractStart = 1462744800; // represents 05/09/2016 00:00:00, use e.g. http://www.timestampconvert.com/ to get your start timestamp
     uint public renewalPeriod = 7; // in days
     uint public requiredMarbles = 2; // of each type, every period
-    uint public allowedUsages = 2; // times the ressource may be used per fulfillment of requirements
     uint public requiredExtraMarbles = 10; // number of extra marbles required for redeeming
     uint[] allowedDays = [1, 4]; // array of days on which the ressource may be used in the following period. If the Contract starts on Monday 00:00, day 0 is monday, day 6 is sunday.
-    
-    // represents a child  of the family
+
     struct Child {
         uint indianredMarbles; // vocabulary
         uint skyblueMarbles; // instrument 
-        uint neonMarbles; // represent extraCredit
-        uint usageTokens; // how many times can the resource be used
+        uint neonMarbles; // represent extra credit
     }
     
     struct Parent {
@@ -21,63 +18,57 @@ contract FamilyResourceControl{
     
     address responsibleParent; // this is the owner of the contract
     
-    // This declares a state variable that
-    // stores a `Child` and `Parent` struct for each possible address.
     mapping(address => Child) children;
     mapping(address => Parent) parents;
     
-    modifier onlyCreator(address _responsibleParent)
-    {
-        if (msg.sender != _responsibleParent)
+    
+    // modifiers
+    modifier onlyOwner {
+        if (msg.sender != responsibleParent)
             throw;
         _
     }
     
+    modifier onlyParent {
+        if (!parents[msg.sender].canAward)
+            throw;
+        _
+    }
+    
+    // constructor
     function FamilyResourceControl(){
-        responsibleParent = msg.sender; // this is the creator of the contract one of the people authorized to award marbles
-        
-        // create the parent
+        responsibleParent = msg.sender; // this is the creator of the contract and the one authorized to add other parents
         parents[responsibleParent].canAward = true;
     }
     
-    function awardIndianredMarble(address goodChild) {
-        awardMarble(0, goodChild);
-    }
-    
-    function awardSkyblueMarble(address goodChild) {
-        awardMarble(1, goodChild);
-    }
-    
+    // cumulative required marbles
     function cumReqMarbles() returns(uint) {
-        return ((now - contractStart)/(renewalPeriod * 86400))*requiredMarbles;
+        return ((now - contractStart)/(renewalPeriod * 1 days))*requiredMarbles;
     }
     
+    // calculate current day of week (first day => day 0)
     function currentDay() returns(uint){
-        return ((now - contractStart)/86400) % (renewalPeriod);
+        return ((now - contractStart)/ 1 days) % (renewalPeriod);
     }
     
-    function awardMarble(uint whatMarble, address goodChild){
-        
-        if (parents[msg.sender].canAward) {
-            if (whatMarble == 0) {
-                if (children[goodChild].indianredMarbles >= cumReqMarbles()+2){
-                    children[goodChild].neonMarbles++;
-                } else {
-                    children[goodChild].indianredMarbles++;
-                }
-            } else if (whatMarble == 1){
-                if (children[goodChild].skyblueMarbles >= cumReqMarbles()+2){
-                    children[goodChild].neonMarbles++;
-                } else {
-                    children[goodChild].skyblueMarbles++;
-                }
+    // award a marble
+    function awardMarble(bytes32 whatMarble, address goodChild) onlyParent {
+        if (whatMarble == "indianredMarble") {
+            if (children[goodChild].indianredMarbles >= cumReqMarbles()+2){
+                children[goodChild].neonMarbles++;
+            } else {
+                children[goodChild].indianredMarbles++;
             }
-            
-        } else {
-            throw;
+        } else if (whatMarble == "skyblueMarble"){
+            if (children[goodChild].skyblueMarbles >= cumReqMarbles()+2){
+                children[goodChild].neonMarbles++;
+            } else {
+                children[goodChild].skyblueMarbles++;
+            }
         }
     }
     
+    // function to be called by the resource controler
     function canUseResource(address goodChild) returns (bool) {
         for (uint i = 0; i < allowedDays.length; i++) {
             if (
@@ -91,37 +82,36 @@ contract FamilyResourceControl{
         return false;
     }
     
-    function useNeonMarbles(address goodChild) returns(bool){
-        if (parents[msg.sender].canAward && children[goodChild].neonMarbles >= requiredExtraMarbles) {
-            children[goodChild].neonMarbles = children[goodChild].neonMarbles - requiredExtraMarbles;
+    // redeem neon marbles
+    function useNeonMarbles(address goodChild) onlyParent returns(bool){
+        if (children[goodChild].neonMarbles >= requiredExtraMarbles) {
+            children[goodChild].neonMarbles -=  requiredExtraMarbles;
             return true;
         } else {
             return false;
         }
     }
     
-    // for debugging
-    function getState(address goodChild) returns(uint, uint, uint, uint){
-        return(
-            children[goodChild].indianredMarbles,
-            children[goodChild].skyblueMarbles,
-            children[goodChild].neonMarbles,
-            children[goodChild].usageTokens);
+    function addParent(address furtherParent) onlyOwner {
+            parents[furtherParent].canAward = true;
     }
     
-    function addParent(address realParent){
-        if (msg.sender == responsibleParent){
-            parents[realParent].canAward = true;
-        } else {
-            throw;
-        }
-    }
-    
-    function removeParent(address realParent){
+    function removeParent(address realParent) onlyOwner{
         if (msg.sender == responsibleParent){
             parents[realParent].canAward = false;
         } else {
             throw;
         }
+    }
+    
+    function marbleBalance(address goodChild) returns(uint indianredMarbles, uint skyblueMarbles, uint neonMarbles){
+        return(
+            children[goodChild].indianredMarbles,
+            children[goodChild].skyblueMarbles,
+            children[goodChild].neonMarbles);
+    }
+    
+    function remove() onlyOwner{
+        selfdestruct(responsibleParent);
     }
 }
